0bf7acf7411d73b677eed2575c69e220f3011bfb
darylstar <62440542+darylstar@users.noreply.github.com>
2023-08-21 21:00:53 +0300

    fix filename [skip ci]

--- start.sh
+++ start.sh
@@ -10,7 +10,7 @@ mapfile -t Array < /tmp/list
 if [ ${#Array[@]} -eq 0 ]; then
     echo "No targets found" && exit 0;
 fi
-]
+
 # software setup
 # yes|sudo add-apt-repository ppa:savoury1/ffmpeg4
 # yes|sudo add-apt-repository ppa:savoury1/ffmpeg5
@@ -35,13 +35,13 @@ echo filename: $filename
 echo path: $path
 
 echo 'COPY SOUND from D'
-echo 'rclone argument: copy gd:abooks/"$line"  /tmp/'
+echo 'rclone argument: copy d:raw/"$line"  /tmp/'
 rclone copy d:raw/"$line"  /tmp/ || continue
 # file -i /tmp/"$fileext"
 echo 'START CONVERING TO Opus'
 ffmpeg -threads $(nproc) -i "$fileext" -s 1334x750 -map 0:0 -map 0:1 -c:v libx265 -b:v 450k -preset fast -c:a libopus -b:a 64k -ac 2 -filter:a "volume=1.5" "/tmp/$filename.x265.mkv" || { echo "Failed to convert file"; exit 1; };
 echo 'COPY RESULT TO D'
-( rclone copy /tmp/"$filename.x265.mkv" d:out/"$path"/ && rclone deletefile "d:raw/$line" && rm -rf /tmp/"$fileext"  /tmp/"$filename.x265.mkv" ) & 
+( rclone copyto /tmp/"$filename.x265.mkv" d:out/"$path"/"$filename.mkv" && rclone deletefile "d:raw/$line" && rm -rf /tmp/"$fileext"  /tmp/"$filename.x265.mkv" ) & 
 echo 'DELETE OLD FILES'
 rclone cleanup d: &
 done

f7f2d82da5824b8ae061d23423089194a276c49e
darylstar <62440542+darylstar@users.noreply.github.com>
2023-08-21 21:29:54 +0300

    fix ffmpeg input

--- start.sh
+++ start.sh
@@ -39,7 +39,7 @@ echo 'rclone argument: copy d:raw/"$line"  /tmp/'
 rclone copy d:raw/"$line"  /tmp/ || continue
 # file -i /tmp/"$fileext"
 echo 'START CONVERING...'
-ffmpeg -threads $(nproc) -i "$fileext" -s 1334x750 -map 0:0 -map 0:1 -c:v libx265 -b:v 450k -preset fast -c:a libopus -b:a 64k -ac 2 -filter:a "volume=1.5" "/tmp/$filename.x265.mkv" || { echo "Failed to convert file"; exit 1; };
+ffmpeg -threads $(nproc) -i /tmp/"$fileext" -s 1334x750 -map 0:0 -map 0:1 -c:v libx265 -b:v 450k -preset fast -c:a libopus -b:a 64k -ac 2 -filter:a "volume=1.5" "/tmp/$filename.x265.mkv" || { echo "Failed to convert file"; exit 1; };
 echo 'COPY RESULT TO D'
 rclone copyto /tmp/"$filename.x265.mkv" d:out/"$path"/"$filename.mkv" && rclone deletefile "d:raw/$line" && rm -rf /tmp/"$fileext"  /tmp/"$filename.x265.mkv"
 echo 'DELETE OLD FILES'

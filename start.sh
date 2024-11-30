37d331d1674170cb3a51e3cb88a2797881bcc914
darylstar <62440542+darylstar@users.noreply.github.com>
2024-11-30 11:11:54 +0200

    cleanup

--- start.sh
+++ start.sh
@@ -48,7 +48,6 @@ rclone copy c1:"$line"  /tmp/ || continue
 
 echo 'START CONVERTING...'
 ffmpeg -threads $(nproc) -i /tmp/"$fileext" -pix_fmt yuv420p10le -s 1334x750 -map 0:0 -map 0:1 -c:v libsvtav1 -preset 5 -svtav1-params fast-decode=1 -b:v 512k  -c:a libopus -b:a 64k -ac 2 -filter:a "volume=1.5" "/tmp/$filename.av1.mkv" || { echo "Failed to convert file"; exit 1; };
-# ffmpeg -threads $(nproc) -i /tmp/"$fileext" -pix_fmt yuv420p10le -s 1334x750 -map 0:0 -map 0:1 -c:v libx265 -x265-params profile=main10 -b:v 450k -preset medium -c:a libopus -b:a 64k -ac 2 -filter:a "volume=1.5" "/tmp/$filename.x265.mkv" || { echo "Failed to convert file"; exit 1; };
 
 echo 'COPY RESULT TO D'
 rclone copyto /tmp/"$filename.av1.mkv" c2:"$filename.mkv" && rclone deletefile "c1:$line" && rm -rf /tmp/"$fileext"  /tmp/"$filename.av1.mkv"
